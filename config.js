c0538e4576472fae38dc50c84eee8c7764a92a20
RoynBosses09 <roynajack303@gmail.com>
2024-09-03 00:41:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "263774918784" 
+global.namaCreator = "Mabhosiedu RoynJK😎👽" 
 global.autoJoin = false 
-global.antilink = false 
+global.antilink = true 
 global.versisc = '6.0' 
 global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
 global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.packname = "Mabhosiedu RoynJK😎👽" 
 global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
 global.jumlah = "5" 
 global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
